b8c91f241c5b7612ee2f9c23720875eef5955327
Jayweek4246c <geraldtang669@gmail.com>
2025-06-05 11:02:09 +0300

    Week05 homework completed

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        
+         // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Kaylin McDonald", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Fredrick Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Simpson", "World History", "The Causes of World War III");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+
     }
 }
